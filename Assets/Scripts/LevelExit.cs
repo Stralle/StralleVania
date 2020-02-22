78c47f6d32ad23bb42a59d87ab8066c4b4470a5d
Strahinja <srodic16@raf.edu.rs>
2020-02-22 18:16:50 +0100

    Added main menu and game over screens. Added flow between levels.

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+
+    [SerializeField]
+    float m_LevelLoadDelay = 2.0f;
+
+    [SerializeField]
+    float m_LevelExitSlowMoFactor = 0.2f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    public IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = m_LevelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(m_LevelLoadDelay);
+        Time.timeScale = 1.0f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
